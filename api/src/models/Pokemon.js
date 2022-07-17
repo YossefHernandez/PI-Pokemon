e8c73636a31e8eb1d739c497ce2d394969a5f64b
YossefHernandez <yossef.her25@gmail.com>
2022-07-17 17:15:39 -0500

    base de datos terminada

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type:  DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    health:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    attack:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    defense:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    velocity:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    weight:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    height:{
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
   });
 };
